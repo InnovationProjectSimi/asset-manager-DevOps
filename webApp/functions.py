e3f5cbe9743a8abb066c620e2c096d125cc9176c
Simraaj <simraajpmcr2020@gmail.com>
2023-03-23 16:32:56 -0700

    comment

--- webApp/functions.py
+++ webApp/functions.py
@@ -1,5 +1,8 @@
+# function to sanitise inputs from forms
 def sanitise_input(input):
+    # Function made to replace characters input whihc could be used in injection attacks
     EMPTY = ""
+    # Return the stripped sanitised input
     return (
         input.replace("'", EMPTY)
         .replace("--", EMPTY)
