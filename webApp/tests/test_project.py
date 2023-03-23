5c71fdf65bc6881a49d5dfb457a4a4b842aa3d7e
Simraaj <simraajpmcr2020@gmail.com>
2023-03-23 05:48:05 -0700

    password hashing tests

---
+++ webApp/tests/test_project.py
@@ -0,0 +1,13 @@
+from webApp.models import User
+from werkzeug.security import generate_password_hash
+
+# Test password hashing is in place
+def test_password_hashing():
+    # Test to ensure the password input is not the same as the database by checking password is hashed with function
+    new_user = User(
+        username="testuser2",
+        password=generate_password_hash("Pizza567!", method="sha256"),
+    )
+
+    assert new_user.password != "Pizza567!"
+    assert generate_password_hash("Pizza567!", method="sha256") != "Pizza567!"
